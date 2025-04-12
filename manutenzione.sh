a3bfbde70c4ca6647dfa58a8554844c99aa9650c
PakyMaiden <pakymaiden92@gmail.com>
2025-04-12 18:47:52 +0200

    Update manutenzione.sh

--- manutenzione.sh
+++ manutenzione.sh
@@ -7,17 +7,19 @@
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-echo -e "${GREEN}[1/4] Aggiornamento lista pacchetti...${NC}"
+echo -e "${GREEN}[1/5] Aggiornamento lista pacchetti...${NC}"
 apt update
 
-echo -e "${GREEN}[2/4] Aggiornamento pacchetti installati...${NC}"
+echo -e "${GREEN}[2/5] Aggiornamento pacchetti installati...${NC}"
 apt upgrade -y
 
-echo -e "${GREEN}[3/4] Rimozione pacchetti non più necessari...${NC}"
+echo -e "${GREEN}[3/5] Rimozione pacchetti non più necessari...${NC}"
 apt autoremove -y
 
-echo -e "${GREEN}[4/4] Pulizia cache dei pacchetti...${NC}"
+echo -e "${GREEN}[4/5] Pulizia cache dei pacchetti...${NC}"
 apt autoclean -y
 
-echo -e "${GREEN}✅ Manutenzione completata con successo.${NC}"
+echo -e "${GREEN}[5/5] Pulizia dei log più vecchi di 30 giorni in /var/log...${NC}"
+find /var/log -type f -mtime +30 -delete
 
+echo -e "${GREEN}✅ Manutenzione completata con successo.${NC}"
